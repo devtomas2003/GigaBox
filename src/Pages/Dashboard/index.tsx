cc2de7d0d89986ee23dc4c8449a1a4686a6c3c99
Tomas Dinis <dev.tomas2003@gmail.com>
2022-08-13 01:36:06 +0100

    Inicio do Header

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,11 +1,80 @@
 import {
-    Container
+    Container,
+    HeaderBox,
+    LeftLogo,
+    ImgLogo,
+    InfoZone,
+    BtnBox,
+    LangSelector,
+    TxtActLang,
+    BoxMenuTp,
+    TopMenu,
+    TxtBtnMenuTop,
+    LftOpts,
+    RighOpts
 } from "./style";
+
+import Logo from "../../resources/big.jpg";
+
+import { TbWorld } from "react-icons/tb";
+import { MdExitToApp, MdArrowDropDown, MdOutlineWifiTethering, MdOutlineSimCard } from "react-icons/md";
+import { GiRotaryPhone } from "react-icons/gi";
+import { AiOutlineWifi, AiFillHome } from "react-icons/ai";
+import { BsGearFill } from "react-icons/bs";
+import { FaTools } from "react-icons/fa";
 
 export default function Dashboard(){
     return (
         <Container>
-
+            <HeaderBox>
+                <LeftLogo>
+                    <ImgLogo src={Logo} title="STAR Labs" alt="Star Labs" />
+                </LeftLogo>
+                <InfoZone>
+                    <LangSelector>
+                        <TxtActLang>English</TxtActLang>
+                        <MdArrowDropDown size={40} color="#444" />
+                    </LangSelector>
+                    <BtnBox>
+                        <AiOutlineWifi size={40} color="#28a745" />
+                    </BtnBox>
+                    <BtnBox>
+                        <GiRotaryPhone size={40} color="#28a745" />
+                    </BtnBox>
+                    <BtnBox>
+                        <TbWorld size={40} color="#28a745" />
+                    </BtnBox>
+                    <BtnBox>
+                        <MdExitToApp size={40} color="#444" />
+                    </BtnBox>
+                </InfoZone>
+            </HeaderBox>
+            <TopMenu>
+                <LftOpts>
+                    <BoxMenuTp>
+                        <AiFillHome size={30} color="#fff" />
+                        <TxtBtnMenuTop>Home</TxtBtnMenuTop>
+                    </BoxMenuTp>
+                    <BoxMenuTp>
+                        <MdOutlineWifiTethering size={30} color="#fff" />
+                        <TxtBtnMenuTop>WiFi</TxtBtnMenuTop>
+                    </BoxMenuTp>
+                    <BoxMenuTp>
+                        <MdOutlineSimCard size={30} color="#fff" />
+                        <TxtBtnMenuTop>WAN</TxtBtnMenuTop>
+                    </BoxMenuTp>
+                    <BoxMenuTp>
+                        <BsGearFill size={30} color="#fff" />
+                        <TxtBtnMenuTop>Avançado</TxtBtnMenuTop>
+                    </BoxMenuTp>
+                </LftOpts>
+                <RighOpts>
+                    <BoxMenuTp>
+                        <FaTools size={30} color="#fff" />
+                        <TxtBtnMenuTop>Avançado</TxtBtnMenuTop>
+                    </BoxMenuTp>     
+                </RighOpts>
+            </TopMenu>
         </Container>
     );
 }
--- src/Pages/Dashboard/style.ts
+++ src/Pages/Dashboard/style.ts
@@ -3,3 +3,75 @@ import styled from "styled-components";
 export const Container = styled.div`
 
 `;
+
+export const HeaderBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+`;
+
+export const LeftLogo = styled.div``;
+
+export const ImgLogo = styled.img`
+    width: 120px;
+`;
+
+export const InfoZone = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const BtnBox = styled.div`
+    &:not(:first-child){
+        margin-left: 20px;
+    }
+    &:hover{
+        cursor: pointer;
+    }
+`;
+
+export const LangSelector = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const TxtActLang = styled.p`
+    font-size: 17px;
+`;
+
+export const TopMenu = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background: #007DFF;
+`;
+
+export const BoxMenuTp = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 15px;
+    &:hover{
+        background: #007DF0;
+        cursor: pointer;
+    }
+`;
+
+export const TxtBtnMenuTop = styled.p`
+    margin-left: 10px;
+    color: #fff;
+    font-size: 18px;
+`;
+
+export const LftOpts = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const RighOpts = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
